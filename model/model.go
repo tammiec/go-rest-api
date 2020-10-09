e1bdfd05951382c452c6f3ea4dd16c5860f927b7
tammiec <tammie.chung@searchspring.com>
2020-10-09 16:37:55 -0400

    add createUser and updateUser methods

--- model/model.go
+++ model/model.go
@@ -12,7 +12,6 @@ type User struct {
 	Id int
 	Name string
 	Email string
-	Password string
 }
 
 func GetDb(dbUrl string) *sql.DB {
@@ -26,7 +25,7 @@ func GetDb(dbUrl string) *sql.DB {
 
 func GetUsers(db *sql.DB) ([]*User, error) {
 	users := make([]*User, 0)
-	rows, err := db.Query("SELECT id, name, email, password FROM users")
+	rows, err := db.Query("SELECT id, name, email FROM users")
 	if err != nil {
 		return nil, err
 	}
@@ -34,7 +33,7 @@ func GetUsers(db *sql.DB) ([]*User, error) {
 
 	for rows.Next() {
 		user := &User{}
-		err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.Password)
+		err := rows.Scan(&user.Id, &user.Name, &user.Email)
 		if err != nil {
 			return nil, err
 		}
@@ -45,12 +44,12 @@ func GetUsers(db *sql.DB) ([]*User, error) {
 
 func GetUser(db *sql.DB, id int) (*User, error) {
 	user := &User{}
-	stmt, err := db.Prepare("SELECT id, name, email, password FROM users WHERE id=$1")
+	stmt, err := db.Prepare("SELECT id, name, email FROM users WHERE id=$1")
 	if err != nil {
 		return nil, err
 	}
 	defer stmt.Close()
-	err = stmt.QueryRow(id).Scan(&user.Id, &user.Name, &user.Email, &user.Password)
+	err = stmt.QueryRow(id).Scan(&user.Id, &user.Name, &user.Email)
 	if err != nil {
 		return nil, err
 	}
@@ -59,12 +58,40 @@ func GetUser(db *sql.DB, id int) (*User, error) {
 
 func DeleteUser(db *sql.DB, id int) (*User, error) {
 	user := &User{}
-	stmt, err := db.Prepare("DELETE FROM users WHERE id=$1 RETURNING id, name, email, password")
+	stmt, err := db.Prepare("DELETE FROM users WHERE id=$1 RETURNING id, name, email")
 	if err != nil {
 		return nil, err
 	}
 	defer stmt.Close()
-	err = stmt.QueryRow(id).Scan(&user.Id, &user.Name, &user.Email, &user.Password)
+	err = stmt.QueryRow(id).Scan(&user.Id, &user.Name, &user.Email)
+	if err != nil {
+		return nil, err
+	}
+	return user, err
+}
+
+func CreateUser(db *sql.DB, name string, email string, password string) (*User, error) {
+	user := &User{}
+	stmt, err := db.Prepare("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(name, email, password).Scan(&user.Id, &user.Name, &user.Email)
+	if err != nil {
+		return nil, err
+	}
+	return user, err
+}
+
+func UpdateUser(db *sql.DB, id int, name string, email string, password string) (*User, error) {
+	user := &User{}
+	stmt, err := db.Prepare("UPDATE users SET name=$1, email=$2, password=$3 WHERE id=$4 RETURNING id, name, email")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(name, email, password, id).Scan(&user.Id, &user.Name, &user.Email)
 	if err != nil {
 		return nil, err
 	}
